1c858f4e83e6272b68f053083e2727d927ba654b
Rae Wiechens <raeannw321@gmail.com>
2024-08-26 19:03:20 -0500

    updateded weather api and page views

--- components/nav.js
+++ components/nav.js
@@ -2,13 +2,18 @@ import html from "html-literal";
 // Example of using a component inside another component
 import navItem from "./navItem.js";
 import navItemBottom from "./navItemBottom.js";
+import logo from "../assets/img/planPalLogo.png";
+import instagram from "../assets/img/Instagram.svg";
+import linkedin from "../assets/img/LinkedIn.svg";
+import github from "../assets/img/github.svg";
+import pin from "../assets/img/Pinterest.svg";
 
 // constructing an HTML list of items from the array in Store
 //  - .map formats the array elements into html
 //      and constructs a new array from the results
 //  - .join joins the elements of the new array into one long string
 //  - data-navigo is a switch that allows Navigo to handle our page routing
-export default (state) => {
+export default state => {
   return html`
     <nav>
       <i class="fas fa-bars"></i>
@@ -17,42 +22,20 @@ export default (state) => {
         <i class="fas fa-bars"></i>
         <ul class="navMain">
           <li class="hidden--mobile nav-links"></li>
-          <li><img id="logo" src="images/planPalLogo.png" /></li>
-          ${state.top.map((item) => navItem(item)).join("")}
-          <!-- <li class="subNavMain">
-    <span class="dot"></span>
-    <h3 class="navText">name</h3>
-  </li>
-  <li class="subNavMain">
-    <span class="dot"></span>
-    <a href="index.html">
-      <h3 class="navText">daily</h3>
-    </a>
-  </li>
-  <li class="subNavMain">
-    <span class="dot"></span>
-    <h3 class="navText">weekly</h3>
-  </li>
-  <li class="subNavMain">
-    <span class="dot"></span>
-    <h3 class="navText">monthly</h3>
-  </li> -->
+          <li><img id="logo" src="${logo}" /></li>
+          ${state.top.map(item => navItem(item)).join("")}
         </ul>
         <hr class="dotted-nav" />
         <ul>
-          ${state.bottom.map((item) => navItemBottom(item)).join("")}
-          <!-- <li><a href="about.html">
-  <h4 class="navTextBottom">about</h4>
-</a></li>
-<li><a href="contact.html">
-  <h4 class="navTextBottom">contact</h4>
-</a></li> -->
+          ${state.bottom.map(item => navItemBottom(item)).join("")}
         </ul>
-        <li><h4 class="navTextBottom">follow us:</h4></li>
-        <li><img class="socialIcon" src="images/Instagram.svg" /></li>
-        <li><img class="socialIcon" src="images/LinkedIn.svg" /></li>
-        <li><img class="socialIcon" src="images/GitHub.svg" /></li>
-        <li><img class="socialIcon" src="images/Pinterest.svg" /></li>
+        <div class="socialIconRow">
+          <li><h4 class="navTextBottom">follow us:</h4></li>
+          <li><img class="socialIcon" src="${instagram}" /></li>
+          <li><img class="socialIcon" src="${linkedin}" /></li>
+          <li><img class="socialIcon" src="${github}" /></li>
+          <li><img class="socialIcon" src="${pin}" /></li>
+        </div>
         <h4 class="navTextBottom">Planpal app © 2024</h4>
       </nav>
     </nav>
---
+++ views/monthly.js
@@ -0,0 +1,38 @@
+import html from "html-literal";
+import { header } from "../components";
+import themeButton from "../assets/img/sliders-solid.svg";
+
+export default state => html`
+  <section>
+    <div class="row">
+      <div class="side">
+        ${header(state)}
+      </div>
+              <button id="themeButton">
+                <img src="${themeButton}" alt="settings">
+              </button>
+            </div>
+            <hr class="dotted"/>
+            <div class="greetingDate">
+              <h2 class="regText">hello name</h2>
+              <h2 class="date regText">date</h2>
+            </div>
+          </div>
+          <div class="contentTwo">
+          <div class="weather">
+              <h2 class="weatherText">${state.weather.temp}°F</h2>
+              <p>${state.weather.city}</p>
+              <p class="weatherText">${state.weather.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="monthly-container">
+    <div id="monthly"></div>
+
+  </div>
+  </div>
+</section>
+`;
